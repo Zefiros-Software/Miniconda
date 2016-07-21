61f6b98bf93d865a82a699109a4ba0dbe1f2bf3d
Paul Visscher <paul.pev@hotmail.com>
2016-07-21 14:46:01 +0200

    Escape fix

--- anaconda.lua
+++ anaconda.lua
@@ -46,7 +46,7 @@ if result:gsub( "conda %d+%.%d+%.%d+", "" ) == result then
         zpm.util.download( "http://repo.continuum.io/archive/Anaconda3-4.1.1-MacOSX-x86_64.sh", zpm.temp, "*" )
         local file = string.format( "%s/%s", zpm.temp, "Anaconda3-4.1.1-MacOSX-x86_64.sh" )
         os.executef( "%s -p $HOME/zpm-anaconda", file )
-        os.execute( "export PATH=%"$HOME/zpm-anaconda/bin:$PATH%"" )
+        os.execute( "export PATH=\"$HOME/zpm-anaconda/bin:$PATH\"" )
 
         os.remove( file )
 
@@ -55,7 +55,7 @@ if result:gsub( "conda %d+%.%d+%.%d+", "" ) == result then
         zpm.util.download( "http://repo.continuum.io/archive/Anaconda3-4.1.1-Linux-x86_64.sh", zpm.temp, "*" )
         local file = string.format( "%s/%s", zpm.temp, "Anaconda3-4.1.1-Linux-x86_64.sh" )
         os.executef( "%s -p $HOME/zpm-anaconda", file )
-        os.execute( "export PATH=%"$HOME/zpm-anaconda/bin:$PATH%"" )
+        os.execute( "export PATH=\"$HOME/zpm-anaconda/bin:$PATH\"" )
 
         os.remove( file )
 
