7f2e56d4062a3b3ee0733a158ff0bdfc3a08988c
Paul Visscher <paul.pev@hotmail.com>
2016-07-21 16:39:50 +0200

    Sysvar fix

--- anaconda.lua
+++ anaconda.lua
@@ -22,9 +22,9 @@
 -- @endcond
 --]]
 
-local anaBin = "$HOME/zpm-anaconda/Scripts/"
+local anaBin = "$(HOME)/zpm-anaconda/Scripts/"
 if os.get() == "windows" then
-    anaBin = "%%UserProfile%%\zpm-anaconda/Scripts/"
+    anaBin = "$(UserProfile)\zpm-anaconda/Scripts/"
 end
 
 local check =  string.format( "%sconda.exe --version", anaBin ) 
