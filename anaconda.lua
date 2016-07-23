0685efa733689187b09a13e8951b9566f8e706f8
Paul Visscher <paul.pev@hotmail.com>
2016-07-23 16:26:04 +0200

    Path join fixed

--- anaconda.lua
+++ anaconda.lua
@@ -35,7 +35,7 @@ if result:gsub( "conda %d+%.%d+%.%d+", "" ) == result then
     if os.get() == "windows" then
 
         zpm.util.download( "http://repo.continuum.io/archive/Anaconda3-4.1.1-Windows-x86_64.exe", zpm.temp, "*" )
-        local file = string.format( "%s/%s", zpm.temp, "Anaconda3-4.1.1-Windows-x86_64.exe" )
+        local file = path.join( zpm.temp, "Anaconda3-4.1.1-Windows-x86_64.exe" )
         zpm.assert( os.isfile(file), "Failed to download anaconda!" )
         os.executef( "start \\wait \"\" %s /InstallationType=JustMe /RegisterPython=0 /S /D=%s\\zpm-anaconda", file, os.getenv("UserProfile") )
         os.remove( file )
