3b73b4b09691db4c2ae75d75ec48c7e30523427c
Paul Visscher <paul.pev@hotmail.com>
2016-07-23 17:17:14 +0200

    Windows debug

--- anaconda.lua
+++ anaconda.lua
@@ -34,7 +34,8 @@ if result:gsub( "conda %d+%.%d+%.%d+", "" ) == result then
 
     if os.get() == "windows" then
 
-        zpm.util.download( "http://repo.continuum.io/archive/Anaconda3-4.1.1-Windows-x86_64.exe", zpm.temp, "*" )
+        print(path.join( zpm.temp, "Anaconda3-4.1.1-Windows-x86_64.exe" ), zpm.temp)
+        --zpm.util.download( "http://repo.continuum.io/archive/Anaconda3-4.1.1-Windows-x86_64.exe", zpm.temp, "*" )
         local file = path.join( zpm.temp, "Anaconda3-4.1.1-Windows-x86_64.exe" ):replace( "/", "\\" )
         os.executef( "start /wait \"\" %s /InstallationType=JustMe /RegisterPython=0 /S /D=%s\\zpm-anaconda", file, os.getenv("UserProfile") )
         os.remove( file )
